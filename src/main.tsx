af97cdde6f3243f8daf0c20c144eac5a2c04bd27
Christopher Robinson <christopher.paul.robinson@ltv.fund>
2024-10-12 22:11:43 -0500

    Add navbar boilerplate from tailwind ui

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Home from './routes/root.tsx';
+import Navigation from './components/Navigation.tsx';
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Navigation isMobile/>
     <RouterProvider router={router} />
   </StrictMode>,
 )
