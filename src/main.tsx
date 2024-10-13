91470b7f16c336df85771ac56afaeb2781886d93
Christopher Robinson <christopher.paul.robinson@ltv.fund>
2024-10-12 20:45:00 -0500

    Add base files and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Home from './routes/root.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
